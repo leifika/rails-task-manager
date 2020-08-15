c93313f723100c64ae03f5a71b4afcffb7945681
Leif Johansen <leif@leifjohansen.com>
2020-08-15 15:42:06 +0200

    task manager 2nd push

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tasks
+  get '/index', to: "tasks#index"
+  get '/new', to: "tasks#new", as: :tasks
+  get '/index/:id', to: "tasks#show", as: :title
+  get '/task/:id', to: "tasks#show", as: :task
+  post '/new', to: "tasks#create"
+
 end
